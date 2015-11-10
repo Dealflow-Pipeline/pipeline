b2b5c0165cc80ac8f1e380622c297ecab5e8e28f
Ryan Haase <ryandhaase@gmail.com>
2015-11-10 11:36:55 -0800

    Routing:
    * Basic routes
    * Controllers
    * HTML pages
    * Installed node globally to run local server `http-server`

---
+++ app/new/new.js
@@ -0,0 +1,8 @@
+var app = angular.module('new', []);
+
+app.controller('newCtrl', [
+  '$scope',
+  function($scope) {
+
+  },
+]);
---
+++ app/startup-profile/startup.js
@@ -0,0 +1,8 @@
+var app = angular.module('startup', []);
+
+app.controller('startupCtrl', [
+  '$scope',
+  function($scope) {
+
+  },
+]);
