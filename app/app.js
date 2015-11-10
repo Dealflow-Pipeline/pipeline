e461abb573134394e9bb01e8e47ef4fe848376f0
Benjamin <bhoffman8@gmail.com>
2015-11-09 17:13:31 -0800

    scaffolding

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('pipeline', [
+  'pipeline.about',
+  'pipeline.auth',
+  'pipeline.dashboard',
+  'pipeline.entrepreneur-dashboard/',
+  'pipeline.entrepreneur-profile/',
+  'pipeline.home',
+  'pipeline.profile',
+  'pipeline.startup-dashboard/',
+  'pipeline.startup-profile/',
+  'ui-router'
+
+
+  ])
