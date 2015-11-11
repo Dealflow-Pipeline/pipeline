6600a2e1ebcb901a2cbb3df0c7c87c789571a039
Benjamin <bhoffman8@gmail.com>
2015-11-10 16:54:56 -0800

    Creates addEntry form + styling

--- app/addEntry/addEntry.js
+++ app/addEntry/addEntry.js
@@ -1,8 +1,28 @@
-var app = angular.module('addEntry', []);
+var app = angular.module('addEntry', [
+  'ui.bootstrap'
+  ]);
 
-app.controller('addEntryCtrl', [
-  '$scope',
-  function($scope) {
+app.controller('addEntryCtrl', function($scope) {
+  console.log($scope);
 
-  },
-]);
+  $scope.startup = {
+    "date": $scope.date
+  };
+  $scope.entrepreneur = {
+    "date": $scope.date
+  };
+
+  $scope.entry = {
+    startup: true,
+    entrepreneur: false
+  };
+
+  $scope.date = new Date();
+
+  $scope.add = function(startup, entrepreneur) {
+    console.log('added!')
+    $scope.startup = angular.copy(startup);
+    $scope.entrepreneur = angular.copy(entrepreneur);
+  };
+
+});
