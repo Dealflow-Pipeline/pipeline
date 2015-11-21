f834b4309251cfb9a2da88bfb07cb301ed0f2fd1
Benjamin <bhoffman8@gmail.com>
2015-11-21 09:15:20 -0800

    AngelList API

--- app/common/angelListFactory.js
+++ app/common/angelListFactory.js
@@ -11,13 +11,13 @@ app.factory('searchAngelListStartups', [
   var companyName = 'periscope';
 
   return $http({
-    method: 'GET',
+    method: 'JSONP',
     url: 'https://api.angel.co/1/search?query=periscope&access_token=00c527015025ccb99080a0e8c9b1b2cff0b8444eab0564f0',
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Content-Type": "application/json"
+      "Content-Type": "application/jsonp"
     },
     // url: 'https://api.angel.co/1/search?query=' + companyName + '&access_token=' + apiKeys.angelList
   })
