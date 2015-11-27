2e4e089d6b975dd9ef071e09e1741e3c14c4e9de
Ryan Haase <ryandhaase@gmail.com>
2015-11-27 10:13:08 -0800

    Cleaned up fullContactFactory.js

--- app/common/fullContactFactory.js
+++ app/common/fullContactFactory.js
@@ -6,22 +6,23 @@ app.factory('fullContactPersonFactory', [
   'apiKeys',
   function($q, $http, apiKeys) {
 
-  return {
-    getPerson: function(personEmail) {
-      var url = 'https://api.fullcontact.com/v2/person.json?email=bart@fullcontact.com&apiKey=' + apiKeys.fullContact;
-      var defer = $q.defer();
-      var url = url;
-      $http.get(url, {
-        headers: {
-          'Content-type': 'application/json'
-        }
-      }).success(function(data) {
-        defer.resolve(data)
-      }).error(function(error) {
-        console.log(error);
-      });
+    return {
+      getPerson: function(personEmail) {
+        var url = 'https://api.fullcontact.com/v2/person.json?email=bart@fullcontact.com&apiKey=' + apiKeys.fullContact;
+        var defer = $q.defer();
+        var url = url;
+        $http.get(url, {
+          headers: {
+            'Content-type': 'application/json',
+          },
+        }).success(function(data) {
+          defer.resolve(data);
+        }).error(function(error) {
+          console.log(error);
+        });
 
-      return defer.promise;
-    }
-  }
-}]);
+        return defer.promise;
+      },
+    };
+  },
+]);
