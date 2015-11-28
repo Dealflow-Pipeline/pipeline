6661a22629160fb0b193f6349eb52ea53736d307
Ryan Haase <ryandhaase@gmail.com>
2015-11-28 15:18:43 -0800

    Code Cleanup:
    * Formatting on js files
    * Removed extra div on addEntry.html

--- app/nav/nav.js
+++ app/nav/nav.js
@@ -4,14 +4,13 @@ app.controller('navCtrl', [
   '$scope',
   '$uibModal',
   function($scope, $uibModal) {
-    console.log('more tests')
 
-  $scope.open = function() {
-    var uibModalInstance = $uibModal.open({
-      animation: true,
-      templateUrl: 'addEntry/addEntry.html',
-      controller: 'addEntryCtrl',
-    });
-  };
-
-}]);
+    $scope.open = function() {
+      var uibModalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'addEntry/addEntry.html',
+        controller: 'addEntryCtrl',
+      });
+    };
+  },
+]);
